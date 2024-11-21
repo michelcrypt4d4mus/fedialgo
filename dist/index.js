2a747722fbf05f269f9e2a24e3f1269443d65b32
ashariyar <ashariyar@users.noreply.github.com>
2024-11-21 05:04:00 -0500

    remove warning

--- dist/index.js
+++ dist/index.js
@@ -78,7 +78,6 @@ class TheAlgorithm {
         scoredFeed = scoredFeed.map((item) => {
             const seconds = Math.floor((new Date().getTime() - new Date(item.createdAt).getTime()) / 1000);
             const timeDiscount = Math.pow((1 + 0.05), -Math.pow((seconds / 3600), 2));
-            console.warn(`Computed timeDiscount for status ${item.uri}: ${timeDiscount}`);
             item.value = (item.value ?? 0) * timeDiscount;
             item.timeDiscount = timeDiscount;
             return item;
