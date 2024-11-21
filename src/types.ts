394559259940b9b4cc71b26284841cb4f81feff6
ashariyar <ashariyar@users.noreply.github.com>
2024-11-21 15:50:11 -0500

    alphabetize StatusType properties

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,28 @@
 import { mastodon } from 'masto';
 
-export type StorageValue = serverFeatureType | accFeatureType | mastodon.v1.Account | weightsType | string
+export type StorageValue = serverFeatureType | accFeatureType | mastodon.v1.Account | weightsType | string;
 
 export interface weightsType {
     [key: string]: number; // Replace 'any' with the desired value type (e.g., string, number, etc.)
-}
+};
 
 export type accFeatureType = {
     [key: mastodon.v1.Account["acct"]]: number; // Replace 'any' with the desired value type (e.g., string, number, etc.)
-}
+};
 
 export type serverFeatureType = {
     [key: mastodon.v1.Instance["uri"]]: number; // Replace 'any' with the desired value type (e.g., string, number, etc.)
-}
+};
 
 export interface StatusType extends mastodon.v1.Status {
-    topPost?: boolean;
+    reblog?: StatusType;
+    reblogBy?: string;
     recommended?: boolean;
     similarity?: number;
     scores?: weightsType;
+    topPost?: boolean;
     value?: number;
-    reblog?: StatusType;
-    reblogBy?: string;
-}
+};
 
 export type FeedFetcher = (api: mastodon.rest.Client) => Promise<StatusType[]>;
 export type Scorer = (api: mastodon.rest.Client, status: StatusType) => number;
