f40fa6981148a9e9539f278642b641ab7b598ac1
ashariyar <ashariyar@users.noreply.github.com>
2024-11-20 22:27:35 -0500

    Fix 'reblogger' typo

--- src/scorer/feature/reblogsFeatureScorer.ts
+++ src/scorer/feature/reblogsFeatureScorer.ts
@@ -8,7 +8,7 @@ export default class reblogsFeatureScorer extends FeatureScorer {
         super({
             featureGetter: (api: mastodon.rest.Client) => { return FeatureStorage.getTopReblogs(api) },
             verboseName: "Reblogs",
-            description: "Posts that are from your most reblogger users",
+            description: "Posts that are from accounts you have retooted the most",
             defaultWeight: 3,
         })
     }
