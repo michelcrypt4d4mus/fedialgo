2aef824206e70e8faf95c9924073b80ff9efd4a5
ashariyar <ashariyar@users.noreply.github.com>
2024-11-21 19:40:33 -0500

    Logging of page retrieval

--- dist/feeds/homeFeed.js
+++ dist/feeds/homeFeed.js
@@ -3,22 +3,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/*
+ * This file is responsible for fetching the array of StatusType objects that make up
+ * the home timeline feed of a user (AKA "what most people see first upon opening Mastodon").
+ */
 const Storage_1 = __importDefault(require("../Storage"));
+const MAX_PAGES = 10;
+const MAX_TIMELINE_HOURS = 24;
+const TIMELINE_LOOKBACK_MS = MAX_TIMELINE_HOURS * 60 * 60 * 1000;
 async function getHomeFeed(api, _user) {
+    let pagesRetrieved = 0;
     let results = [];
-    let pages = 10;
+    // Don't load posts more than MAX_TIMELINE_HOURS old.
     const lastOpened = new Date((await Storage_1.default.getLastOpened() ?? 0) - 600);
-    const defaultCutoff = new Date(Date.now() - 43200000);
-    const dateCutoff = lastOpened < defaultCutoff ? defaultCutoff : lastOpened;
-    console.log("Date Cutoff: ", dateCutoff);
+    const cutoffTimelineAt = new Date(Date.now() - TIMELINE_LOOKBACK_MS);
+    const timelineCutoff = lastOpened < cutoffTimelineAt ? cutoffTimelineAt : lastOpened;
+    console.log("timelineCutoff: ", timelineCutoff);
     for await (const page of api.v1.timelines.home.list()) {
         results = results.concat(page);
-        pages--;
-        //check if status is less than 12 hours old
-        if (pages === 0 || new Date(page[0].createdAt) < dateCutoff) {
+        pagesRetrieved++;
+        console.log(`ulled page ${pagesRetrieved} of home feed w/${page.length} toots...`);
+        // break if we've pulled MAX_PAGES pages status is older than MAX_TIMELINE_HOURS old
+        if (pagesRetrieved == MAX_PAGES || new Date(page[0].createdAt) < timelineCutoff) {
+            console.log(`Breaking out of getHomeFeed loop after ${pagesRetrieved} pages`);
             break;
         }
     }
     return results;
 }
 exports.default = getHomeFeed;
+;
--- src/feeds/homeFeed.ts
+++ src/feeds/homeFeed.ts
@@ -1,21 +1,37 @@
-import { mastodon } from "masto";
+/*
+ * This file is responsible for fetching the array of StatusType objects that make up
+ * the home timeline feed of a user (AKA "what most people see first upon opening Mastodon").
+ */
 import Storage from "../Storage";
+import { mastodon } from "masto";
 import { StatusType } from "../types";
 
+const MAX_PAGES = 10;
+const MAX_TIMELINE_HOURS = 24;
+const TIMELINE_LOOKBACK_MS = MAX_TIMELINE_HOURS * 60 * 60 * 1000;
+
+
 export default async function getHomeFeed(api: mastodon.rest.Client, _user: mastodon.v1.Account): Promise<StatusType[]> {
+    let pagesRetrieved = 0;
     let results: StatusType[] = [];
-    let pages = 10;
-    const lastOpened = new Date((await Storage.getLastOpened() ?? 0) - 600)
-    const defaultCutoff = new Date(Date.now() - 43200000)
-    const dateCutoff = lastOpened < defaultCutoff ? defaultCutoff : lastOpened
-    console.log("Date Cutoff: ", dateCutoff)
+
+    // Don't load posts more than MAX_TIMELINE_HOURS old.
+    const lastOpened = new Date((await Storage.getLastOpened() ?? 0) - 600);
+    const cutoffTimelineAt = new Date(Date.now() - TIMELINE_LOOKBACK_MS);
+    const timelineCutoff = lastOpened < cutoffTimelineAt ? cutoffTimelineAt : lastOpened;
+    console.log("timelineCutoff: ", timelineCutoff);
+
     for await (const page of api.v1.timelines.home.list()) {
         results = results.concat(page as StatusType[]);
-        pages--;
-        //check if status is less than 12 hours old
-        if (pages === 0 || new Date(page[0].createdAt) < dateCutoff) {
+        pagesRetrieved++;
+        console.log(`ulled page ${pagesRetrieved} of home feed w/${page.length} toots...`);
+
+        // break if we've pulled MAX_PAGES pages status is older than MAX_TIMELINE_HOURS old
+        if (pagesRetrieved == MAX_PAGES || new Date(page[0].createdAt) < timelineCutoff) {
+            console.log(`Breaking out of getHomeFeed loop after ${pagesRetrieved} pages`);
             break;
         }
     }
+
     return results;
-}
+};
