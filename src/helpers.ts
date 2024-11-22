a025a1028fb8af57bb33f68b3b68fb4cf8a014f7
ashariyar <ashariyar@users.noreply.github.com>
2024-11-22 03:45:46 -0500

    Standardize FROM and retootOf

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { camelCase } from "change-case";
 import { StatusType } from "./types";
 
-const CONTENT_CHARS_TO_LOG = 150;
+const MAX_CONTENT_CHARS = 150;
 
 
 //Masto does not support top posts from foreign servers, so we have to do it manually
@@ -48,21 +48,24 @@ export const mastodonFetch = async <T>(server: string, endpoint: string): Promis
 
 // Returns a simplified version of the status for logging
 export const condensedStatus = (status: StatusType) => {
+    // Contents of post (the text)
     let content = status.reblog?.content || status.content || "";
-    if (content.length > CONTENT_CHARS_TO_LOG) content = content.slice(0, CONTENT_CHARS_TO_LOG) + '...';
-
+    if (content.length > MAX_CONTENT_CHARS) content = `${content.slice(0, MAX_CONTENT_CHARS)}...`;
+    // Account info
+    let accountLabel = describePoster(status);
+    if (status.reblog) accountLabel += ` ｟⬆️⬆️RETOOT of ${describePoster(status.reblog)}⬆️⬆️｠`;
+    // Attachment info
     let mediaAttachments = status.mediaAttachments.map(attachment => attachment.type);
     if (mediaAttachments.length == 0) mediaAttachments = [];
-    let tooterLabel = describePoster(status);
-    if (status.reblog) tooterLabel += ` ｟⬆️⬆️RETOOT of ${describePoster(status.reblog)}⬆️⬆️｠`;
 
     const statusObj = {
-        FROM: `${tooterLabel} [${status.createdAt}]`,
+        FROM: `${accountLabel} [${status.createdAt}]`,
         URL: status.url,
         content: content,
-        retootOf: status.reblog ? `${status.reblog.account.acct} (${status.reblog.createdAt})` : null,
+        retootOf: status.reblog ? `${describePoster(status.reblog)} (${status.reblog.createdAt})` : null,
         inReplyToId: status.inReplyToId,
         mediaAttachments: mediaAttachments,
+        raw: status,
 
         properties: {
             favouritesCount: status.favouritesCount,
@@ -72,14 +75,12 @@ export const condensedStatus = (status: StatusType) => {
         },
 
         score: {
-            timeWeightedScore: status.value,
             rawScore: status.rawScore,
-            timeDiscount: status.timeDiscount,
             scoreComponents: status.scores,
             scoreComponentsWeighted: status.weightedScores,
+            timeDiscount: status.timeDiscount,
+            timeWeightedScore: status.value,
         },
-
-        raw: status,
     };
 
     return Object.keys(statusObj)
@@ -90,6 +91,5 @@ export const condensedStatus = (status: StatusType) => {
 
 // Build a string that contains the display name, account name, etc. for a given post.
 const describePoster = (status: StatusType): string => {
-    // if (!status?.account) return null;
     return `${status.account.displayName} (${status.account.acct})`;
 };
