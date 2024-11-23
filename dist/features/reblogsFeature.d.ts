dcd3e710d0339c4c67f4e4b70695da8f66588189
ashariyar <ashariyar@users.noreply.github.com>
2024-11-23 01:09:56 -0500

    Merge master

--- dist/features/reblogsFeature.d.ts
+++ dist/features/reblogsFeature.d.ts
@@ -1,2 +1,2 @@
 import { mastodon } from "masto";
-export default function reblogsFeature(api: mastodon.rest.Client, user: mastodon.v1.Account): Promise<Record<string, number>>;
+export default function getReblogsFeature(api: mastodon.rest.Client, user: mastodon.v1.Account): Promise<Record<string, number>>;
