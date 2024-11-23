a32d7289367ae382cde1810d1770e3970c20236b
ashariyar <ashariyar@users.noreply.github.com>
2024-11-23 00:21:12 -0500

    diversityFeedScorer code style

--- dist/scorer/feed/diversityFeedScorer.js
+++ dist/scorer/feed/diversityFeedScorer.js
@@ -3,24 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/*
+ * Generates a NEGATIVE score based on how many times the tooter has tooted recently to help
+ * prevent prolific tooters from clogging up the feed.
+ */
 const FeedScorer_1 = __importDefault(require("../FeedScorer"));
 class diversityFeedScorer extends FeedScorer_1.default {
     constructor() {
         super("Diversity", "Disfavor posts from users that you have seen a lot of posts from already");
     }
     feedExtractor(feed) {
-        // this prevents just always the first post from being shown
-        const sortRandom = () => Math.random() - 0.5;
-        return feed.sort(sortRandom).reduce((obj, toot) => {
-            obj[toot.account.acct] = (obj[toot.account.acct] || 0) - 1;
-            return obj;
+        const sortRandom = () => Math.random() - 0.5; // this prevents just always the first post from being shown
+        return feed.sort(sortRandom).reduce((userTootCounts, toot) => {
+            userTootCounts[toot.account.acct] = (userTootCounts[toot.account.acct] || 0) - 1;
+            return userTootCounts;
         }, {});
     }
+    // Note that the penalty for frequent tooters decreases by 1 each time a toot is scored
     async score(toot) {
-        super.score(toot);
-        const frequ = this.features[toot.account.acct];
-        this.features[toot.account.acct] = frequ + 1;
-        return frequ + 1;
+        super.score(toot); // Check if ready
+        this.features[toot.account.acct] += 1;
+        return this.features[toot.account.acct];
     }
 }
 exports.default = diversityFeedScorer;
