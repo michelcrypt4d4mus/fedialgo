dcf65893b4bf8f2e67875d3ca34923be7f155abf
ashariyar <ashariyar@users.noreply.github.com>
2024-11-23 21:52:51 -0500

    filter before sort

--- src/feeds/topPostsFeed.ts
+++ src/feeds/topPostsFeed.ts
@@ -19,12 +19,11 @@ const TRENDING_TOOTS_REST_PATH = "api/v1/trends/statuses";
 
 export default async function topPostsFeed(api: mastodon.rest.Client): Promise<Toot[]> {
     const coreServers = await FeatureStore.getCoreServer(api);
-    console.log(`coreServers: `, coreServers);
 
     // Get list of top mastodon servers // TODO: what does "top" mean here?
     const topServerDomains = Object.keys(coreServers)
+                                   .filter(s => s !== "undefined" && typeof s !== "undefined" && s.length > 0)
                                    .sort((a, b) => (coreServers[b] - coreServers[a]))  // TODO: wtf is this comparison?
-                                   .filter(s => s !== "undefined" && typeof s !== "undefined" && s.length > 0)  // Remove weird records
                                    .slice(0, NUM_SERVERS_TO_POLL);
 
     if (topServerDomains.length == 0) {
