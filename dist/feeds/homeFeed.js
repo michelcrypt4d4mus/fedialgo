4c9f99224406fe97a1b0c034eb52ec9176e9ccd4
ashariyar <ashariyar@users.noreply.github.com>
2024-11-23 20:48:31 -0500

    rename results to toots

--- dist/feeds/homeFeed.js
+++ dist/feeds/homeFeed.js
@@ -11,7 +11,7 @@ const TIMELINE_LOOKBACK_MS = MAX_TIMELINE_HOURS * 60 * 60 * 1000;
 const LAST_OPENED_LOOKBACK_MS = 60 * 1000; // Lookback an extra minute beyond last opened time just in case
 async function getHomeFeed(api, _user) {
     let pagesRetrieved = 0;
-    let results = [];
+    let toots = [];
     // We should already have toots cached up until the last time this app was opened so we
     // don't need to re-retrieve them but in any case never load toots more than MAX_TIMELINE_HOURS old.
     const lastOpenedAt = new Date((await Storage_1.default.getLastOpenedTimestamp() ?? 0) - LAST_OPENED_LOOKBACK_MS);
@@ -20,19 +20,19 @@ async function getHomeFeed(api, _user) {
     console.log("timelineCutoff: ", timelineCutoff);
     // TODO: this didn't quite work with mastodonFetchPages() but it probably could
     for await (const page of api.v1.timelines.home.list({ limit: NUM_TOOTS_PER_PAGE })) {
-        results = results.concat(page);
+        toots = toots.concat(page);
         console.log(`Retrieved page ${++pagesRetrieved} of home feed with ${page.length} toots...`);
         const oldestTootAt = new Date(page[0].createdAt); // TODO: are we sure this is the oldest toot in the page?
         // break if we've pulled MAX_PAGES pages status is older than MAX_TIMELINE_HOURS old
         if (pagesRetrieved == MAX_PAGES || oldestTootAt < timelineCutoff) {
             if (oldestTootAt < timelineCutoff) {
                 console.log(`Halting getHomeFeed() after ${pagesRetrieved} pages bc oldestTootAt='${oldestTootAt}'`);
-                results.forEach((toot, i) => console.log(`timeline toot #${i} was tooted at ${toot.createdAt}`));
+                toots.forEach((toot, i) => console.log(`timeline toot #${i} was tooted at ${toot.createdAt}`));
             }
             break;
         }
     }
-    return results;
+    return toots;
 }
 exports.default = getHomeFeed;
 ;
