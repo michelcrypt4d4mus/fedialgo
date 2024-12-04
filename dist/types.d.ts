54930510d0c26f98ec16137ea7d024690e11869c
ashariyar <ashariyar@users.noreply.github.com>
2024-12-04 04:36:30 -0500

    style

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -66,4 +66,4 @@ export interface TrendingTag extends mastodon.v1.Tag {
     numAccounts?: number;
 }
 export type FeedFetcher = (api: mastodon.rest.Client) => Promise<Toot[]>;
-export type StorageValue = AccountFeature | AccountNames | FeedFilterSettings | StringNumberDict | ServerFeature | TootURIs | Toot[] | mastodon.v1.Account | mastodon.v1.Account[] | number;
+export type StorageValue = AccountFeature | FeedFilterSettings | StringNumberDict | ServerFeature | TootURIs | Toot[] | mastodon.v1.Account | mastodon.v1.Account[] | number;
--- src/types.ts
+++ src/types.ts
@@ -83,5 +83,5 @@ export interface TrendingTag extends mastodon.v1.Tag {
 
 export type FeedFetcher = (api: mastodon.rest.Client) => Promise<Toot[]>;
 
-export type StorageValue = AccountFeature | AccountNames | FeedFilterSettings | StringNumberDict |
-    ServerFeature |TootURIs | Toot[] | mastodon.v1.Account | mastodon.v1.Account[] | number;
+export type StorageValue = AccountFeature | FeedFilterSettings | StringNumberDict | ServerFeature |
+    TootURIs | Toot[] | mastodon.v1.Account | mastodon.v1.Account[] | number;
