911c5ad70cdc745a9e2893e992e444e8ce7674dd
ashariyar <ashariyar@users.noreply.github.com>
2024-12-08 02:24:30 -0500

    style

--- dist/api/objects/account.js
+++ dist/api/objects/account.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildAccountNames = void 0;
+// Build a dict from the acct (e.g @user@server.com) to the Account object for easy lookup
 function buildAccountNames(accounts) {
     return accounts.reduce((accountNames, account) => {
         accountNames[account.acct] = account;
--- src/api/objects/account.ts
+++ src/api/objects/account.ts
@@ -6,6 +6,7 @@ import { mastodon } from "masto";
 import { AccountNames } from "../../types";
 
 
+// Build a dict from the acct (e.g @user@server.com) to the Account object for easy lookup
 export function buildAccountNames(accounts: mastodon.v1.Account[]): AccountNames {
     return accounts.reduce(
         (accountNames, account) => {
