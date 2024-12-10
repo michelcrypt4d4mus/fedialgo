e032bb61a404a70beeddc787ccbf6061d600bf1d
ashariyar <ashariyar@users.noreply.github.com>
2024-12-10 18:06:44 -0500

    rename local vars

--- dist/api/objects/tag.js
+++ dist/api/objects/tag.js
@@ -6,17 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.decorateTrendingTag = void 0;
 const Storage_1 = __importDefault(require("../../Storage"));
 // Lowercase the tag text; inject toot / account counts summed over last NUM_DAYS_TO_COUNT_TAG_DATA.
-function decorateTrendingTag(_tag) {
-    let tag = _tag;
-    tag.name = tag.name.toLowerCase();
-    if (!tag?.history || tag.history.length == 0) {
-        console.warn(`decorateTrendingTag() found no history for tag:`, tag);
-        tag.history = [];
+function decorateTrendingTag(tag) {
+    const trendingTag = tag;
+    trendingTag.name = trendingTag.name.toLowerCase();
+    if (!trendingTag?.history || trendingTag.history.length == 0) {
+        console.warn(`decorateTrendingTag() found no history for tag:`, trendingTag);
+        trendingTag.history = [];
     }
-    const recentHistory = tag.history.slice(0, Storage_1.default.getConfig().numDaysToCountTrendingTagData);
-    tag.numToots = recentHistory.reduce((total, h) => total + parseInt(h.uses), 0);
-    tag.numAccounts = recentHistory.reduce((total, h) => total + parseInt(h.accounts), 0);
-    return tag;
+    const recentHistory = trendingTag.history.slice(0, Storage_1.default.getConfig().numDaysToCountTrendingTagData);
+    trendingTag.numToots = recentHistory.reduce((total, h) => total + parseInt(h.uses), 0);
+    trendingTag.numAccounts = recentHistory.reduce((total, h) => total + parseInt(h.accounts), 0);
+    return trendingTag;
 }
 exports.decorateTrendingTag = decorateTrendingTag;
 ;
