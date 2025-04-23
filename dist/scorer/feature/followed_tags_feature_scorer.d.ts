0c514bb5d43aca7a2cfaf7d89a1f181f9e6bb947
ashariyar <ashariyar@users.noreply.github.com>
2025-04-23 16:46:35 -0400

    remove unused followed_tags_feature_scorer dist files

--- dist/scorer/feature/followed_tags_feature_scorer.d.ts
+++
@@ -1,8 +0,0 @@
-import FeatureScorer from '../feature_scorer';
-import Toot from '../../api/objects/toot';
-import { StringNumberDict } from '../../types';
-export default class FollowedTagsFeatureScorer extends FeatureScorer {
-    constructor();
-    featureGetter(): Promise<StringNumberDict>;
-    _score(toot: Toot): Promise<number>;
-}
