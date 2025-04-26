98c4afeacf79f248ccaab03f11dd544597d3516c
ashariyar <ashariyar@users.noreply.github.com>
2025-04-25 22:19:42 -0400

    Reduce maxTimelineTootsToFetch to 1000, incrementalLoadDelayMS to 1500

--- dist/config.js
+++ dist/config.js
@@ -111,8 +111,8 @@ exports.DEFAULT_CONFIG = {
     enableIncrementalLoad: true,
     // incrementalLoadDelayMS: 500,         // Delay between incremental loads of toots
     // maxTimelineTootsToFetch: 2_500,      // How many standard timeline toots to pull
-    incrementalLoadDelayMS: 2000,
-    maxTimelineTootsToFetch: 1200,
+    incrementalLoadDelayMS: 1500,
+    maxTimelineTootsToFetch: 1000,
     maxTimelineHoursToFetch: 168,
     numTootsInFirstFetch: 80,
     // API stuff
