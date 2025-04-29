b6860beb44fb23c6ae71e230386f49c7c1377c70
ashariyar <ashariyar@users.noreply.github.com>
2025-04-29 15:37:19 -0400

    Remove comment

--- src/scorer/scorer.ts
+++ src/scorer/scorer.ts
@@ -69,7 +69,6 @@ export default abstract class Scorer {
 
         // Multiple weighted score by time decay penalty to get a final weightedScore
         const timeDecayWeight = userWeights[WeightName.TIME_DECAY] || DEFAULT_WEIGHTS[WeightName.TIME_DECAY];
-        // const timeDecayMultiplier = 1.0 / Math.pow(tootToScore.ageInHours(), timeDecayWeight);
         const decayExponent = -1 * Math.pow(toot.ageInHours(), Storage.getConfig().timelineDecayExponent);
         const timeDecayMultiplier = Math.pow(timeDecayWeight + 1, decayExponent);
         const weightedScore = this.sumScores(weightedScores);
