73103a499a5ab19c8584aa27e73ea1475e91683b
ashariyar <ashariyar@users.noreply.github.com>
2025-05-14 12:19:58 -0400

    debug config

--- src/config.ts
+++ src/config.ts
@@ -454,8 +454,8 @@ if (isQuickMode) {
 }
 
 if (isDebugMode) {
-    Config.apiDefaults[StorageKey.NOTIFICATIONS]!.numMinutesUntilStale = 60;
-    Config.apiDefaults[StorageKey.RECENT_USER_TOOTS]!.numMinutesUntilStale = 60;
+    Config.apiDefaults[StorageKey.NOTIFICATIONS]!.numMinutesUntilStale = 10;
+    Config.apiDefaults[StorageKey.RECENT_USER_TOOTS]!.numMinutesUntilStale = 10;
     Config.maxRecordsForFeatureScoring = 20_000;
 };
 
