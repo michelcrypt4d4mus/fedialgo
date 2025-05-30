394465c9df75a1d8ddd46bef451ffc126c8a02d3
ashariyar <ashariyar@users.noreply.github.com>
2025-05-30 15:41:27 -0400

    Update changelog; add git commit script

--- git-commit.sh
+++ git-commit.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# The git commit hook doesn't actually seem to trigger *BEFORE* the commit so this is a temporary workaround
+set -e
+
 npm run build 
 git commit -am"$1" --no-verify 
 git push
