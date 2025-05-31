bfe0e35ca127cb0a8749c563fa6d4316debc45e3
ashariyar <ashariyar@users.noreply.github.com>
2025-05-30 20:36:59 -0400

    Move ComponentLogger up

--- dist/helpers/log_helpers.d.ts
+++ dist/helpers/log_helpers.d.ts
@@ -2,21 +2,6 @@ import { Mutex, MutexInterface, Semaphore, SemaphoreInterface } from 'async-mute
 export declare const BACKFILL_FEED = "triggerHomeTimelineBackFill()";
 export declare const PREP_SCORERS = "prepareScorers()";
 export declare const TRIGGER_FEED = "triggerFeedUpdate()";
-export declare function lockExecution(locker: Mutex | Semaphore, logPrefix: string): Promise<MutexInterface.Releaser | SemaphoreInterface.Releaser>;
-export declare function logAndThrowError(message: string, obj?: any): never;
-export declare function logTelemetry(logPrefix: string, msg: string, startedAt: Date, ...args: any[]): void;
-export declare function logTootRemoval(prefix: string, tootType: string, numRemoved: number, numTotal: number): void;
-export declare function traceLog(msg: string, ...args: any[]): void;
-export declare const strBytes: (str: string) => number;
-export declare class WaitTime {
-    avgMsPerRequest: number;
-    milliseconds: number;
-    numRequests: number;
-    startedAt: Date;
-    markStart(): void;
-    markEnd(): void;
-    toDict(): Record<string, number>;
-}
 export declare class ComponentLogger {
     componentName: string;
     logPrefix: string;
@@ -32,4 +17,19 @@ export declare class ComponentLogger {
     private getErrorMessage;
     private makeErrorMsg;
     private makeMsg;
+}
+export declare function lockExecution(locker: Mutex | Semaphore, logPrefix: string): Promise<MutexInterface.Releaser | SemaphoreInterface.Releaser>;
+export declare function logAndThrowError(message: string, obj?: any): never;
+export declare function logTelemetry(logPrefix: string, msg: string, startedAt: Date, ...args: any[]): void;
+export declare function logTootRemoval(prefix: string, tootType: string, numRemoved: number, numTotal: number): void;
+export declare function traceLog(msg: string, ...args: any[]): void;
+export declare const strBytes: (str: string) => number;
+export declare class WaitTime {
+    avgMsPerRequest: number;
+    milliseconds: number;
+    numRequests: number;
+    startedAt: Date;
+    markStart(): void;
+    markEnd(): void;
+    toDict(): Record<string, number>;
 }
--- src/helpers/log_helpers.ts
+++ src/helpers/log_helpers.ts
@@ -14,6 +14,77 @@ export const PREP_SCORERS = "prepareScorers()";
 export const TRIGGER_FEED = "triggerFeedUpdate()";
 
 
+// Log lines with "[DEMO APP] <Subtitle> (subsubtitle)" prefix
+export class ComponentLogger {
+    componentName: string;
+    logPrefix: string;
+    subtitle?: string;
+    subsubtitle?: string;
+
+    constructor(componentName: string, subtitle?: string, subsubtitle?: string) {
+        this.componentName = componentName;
+        this.subtitle = subtitle;
+        this.subsubtitle = subsubtitle;
+
+        this.logPrefix = bracketed(componentName) + (subtitle ? ` <${subtitle}>` : "");
+        this.logPrefix += (subsubtitle ? ` (${subsubtitle})` : "");
+    }
+
+    // If first arg is a string, check if 2nd arg is an Error and do some special formatting
+    // Returns the error message in case it's of use.
+    error(msg: string | Error, ...args: any[]): string {
+        if (msg instanceof Error) {
+            console.error(this.makeMsg(msg.message), ...args);
+            return msg.message;
+        }
+
+        msg = this.getErrorMessage(msg, ...args);
+        console.error(this.makeMsg(msg), ...args);
+        return msg;
+    }
+
+    // Also checks the first argument for an Error but first arg must be a string
+    warn(msg: string, ...args: any[]) {
+        msg = this.getErrorMessage(msg, ...args);
+        console.warn(this.makeMsg(msg), ...args);
+    }
+
+    log(msg: string, ...args: any[]) {
+        console.log(this.makeMsg(msg), ...args);
+    }
+
+    info(msg: string, ...args: any[]) {
+        console.info(this.makeMsg(msg), ...args);
+    }
+
+    debug(msg: string, ...args: any[]) {
+        console.debug(this.makeMsg(msg), ...args);
+    }
+
+    // Only writes logs when FEDIALGO_DEBUG env var is set
+    trace(msg: string, ...args: any[]) {
+        isDebugMode && this.debug(msg, ...args);
+    }
+
+    // Mutates args array to pop the first Error if it exists
+    private getErrorMessage(msg: string, ...args: any[]): string {
+        if (args[0] instanceof Error) {
+            return this.makeErrorMsg(args.shift() as Error, msg);
+        } else {
+            return msg;
+        }
+    }
+
+    private makeErrorMsg(error: Error, msg?: string): string {
+        return msg ? `${msg} (error.message="${error.message}")` : error.message;
+    }
+
+    private makeMsg(msg: string | undefined): string {
+        return this.logPrefix + isEmptyStr(msg) ? '' : ` ${msg}`;
+    }
+};
+
+
 // Lock a Semaphore or Mutex and log the time it took to acquire the lock
 export async function lockExecution(
     locker: Mutex | Semaphore,
@@ -120,78 +191,5 @@ export class WaitTime {
             milliseconds: this.milliseconds,
             numRequests: this.numRequests
         };
-    }
-};
-
-
-
-// Log lines with "[DEMO APP] <ComponentName>" prefixed
-// TODO: Move to its own file
-export class ComponentLogger {
-    componentName: string;
-    logPrefix: string;
-    subtitle?: string;
-    subsubtitle?: string;
-
-    constructor(componentName: string, subtitle?: string, subsubtitle?: string) {
-        this.componentName = componentName;
-        this.subtitle = subtitle;
-        this.subsubtitle = subsubtitle;
-
-        this.logPrefix = bracketed(componentName) + (subtitle ? ` <${subtitle}>` : "");
-        this.logPrefix += (subsubtitle ? ` (${subsubtitle})` : "");
-    }
-
-    // If first arg is a string, check if 2nd arg is an Error and do some special formatting
-    // Returns the error message in case it's of use.
-    error(msg: string | Error, ...args: any[]): string {
-        if (msg instanceof Error) {
-            console.error(this.makeMsg(msg.message), ...args);
-            return msg.message;
-        }
-
-        msg = this.getErrorMessage(msg, ...args);
-        console.error(this.makeMsg(msg), ...args);
-        return msg;
-    }
-
-    // Also checks the first argument for an Error but first arg must be a string
-    warn(msg: string, ...args: any[]) {
-        msg = this.getErrorMessage(msg, ...args);
-        console.warn(this.makeMsg(msg), ...args);
-    }
-
-    log(msg: string, ...args: any[]) {
-        console.log(this.makeMsg(msg), ...args);
-    }
-
-    info(msg: string, ...args: any[]) {
-        console.info(this.makeMsg(msg), ...args);
-    }
-
-    debug(msg: string, ...args: any[]) {
-        console.debug(this.makeMsg(msg), ...args);
-    }
-
-    // Only writes logs when FEDIALGO_DEBUG env var is set
-    trace(msg: string, ...args: any[]) {
-        isDebugMode && this.debug(msg, ...args);
-    }
-
-    // Mutates args array to pop the first Error if it exists
-    private getErrorMessage(msg: string, ...args: any[]): string {
-        if (args[0] instanceof Error) {
-            return this.makeErrorMsg(args.shift() as Error, msg);
-        } else {
-            return msg;
-        }
-    }
-
-    private makeErrorMsg(error: Error, msg?: string): string {
-        return msg ? `${msg} (error.message="${error.message}")` : error.message;
-    }
-
-    private makeMsg(msg: string | undefined): string {
-        return this.logPrefix + isEmptyStr(msg) ? '' : ` ${msg}`;
     }
 };
