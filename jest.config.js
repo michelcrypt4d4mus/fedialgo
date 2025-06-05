6f7de55b58dae3b35308d09dbf31f0ca43c0e489
ashariyar <ashariyar@users.noreply.github.com>
2025-06-05 16:23:10 -0400

    Bump some @babel preset packages

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-
-    // Any other configurations you might want.
+    transformIgnorePatterns: ['/node_modules/(?!change-case/)'],
+    // transformIgnorePatterns: ['/node_modules/change-case/'],
 };
