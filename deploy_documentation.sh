4ebd4481f3f071a03fc387d95fe578b08704c97e
ashariyar <ashariyar@users.noreply.github.com>
2025-06-08 21:27:03 -0400

    deploy_documentation.sh

---
+++ deploy_documentation.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash -l
+# This script deploys the documentation to the github_pages branch of the repository.
+set -e
+
+MASTER_BRANCH="master"
+GITHUB_PAGES_BRANCH="github_pages"
+
+assert_repo_is_ready() {
+    git_check_master_branch
+
+    if [[ ! any_uncommitted_changes ]]; then
+        echo "There are uncommitted changes. Please commit or stash them before running this script."
+        exit 1
+    fi
+}
+
+
+assert_repo_is_ready
+git checkout $GITHUB_PAGES_BRANCH
+git merge $MASTER_BRANCH --no-edit
+set +e
+npm run docs
+set -e
+git commit -am"Build documentation"
+git push origin $GITHUB_PAGES_BRANCH
+git checkout $MASTER_BRANCH
+
+echo "Documentation deployed to $GITHUB_PAGES_BRANCH branch."
