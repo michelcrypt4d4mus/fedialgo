2b577b5b8e52ff39fa6e4d4abf0486eec79ec51a
ashariyar <ashariyar@users.noreply.github.com>
2025-06-11 19:07:01 -0400

    Remove Logger from filter.ts

--- dist/api/objects/filter.js
+++ dist/api/objects/filter.js
@@ -1,12 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.extractMutedKeywords = exports.buildMutedRegex = void 0;
-const logger_1 = __importDefault(require("../../helpers/logger"));
 const string_helpers_1 = require("../../helpers/string_helpers");
-const logger = new logger_1.default(`filter.ts`);
 function buildMutedRegex(serverSideFilters) {
     return (0, string_helpers_1.wordsRegex)(extractMutedKeywords(serverSideFilters));
 }
@@ -15,7 +10,7 @@ exports.buildMutedRegex = buildMutedRegex;
 function extractMutedKeywords(serverSideFilters) {
     let keywords = serverSideFilters.map(f => f.keywords.map(k => k.keyword)).flat().flat().flat();
     keywords = keywords.map(k => k.toLowerCase().replace(/^#/, ""));
-    logger.trace(`<mutedKeywords()> found ${keywords.length} keywords:`, keywords);
+    // logger.trace(`<mutedKeywords()> found ${keywords.length} keywords:`, keywords);
     return keywords;
 }
 exports.extractMutedKeywords = extractMutedKeywords;
--- src/api/objects/filter.ts
+++ src/api/objects/filter.ts
@@ -3,10 +3,7 @@
  */
 import { mastodon } from "masto";
 
-import Logger from "../../helpers/logger";
 import { wordsRegex } from "../../helpers/string_helpers";
-
-const logger = new Logger(`filter.ts`);
 
 
 export function buildMutedRegex(serverSideFilters: mastodon.v2.Filter[]): RegExp {
@@ -17,6 +14,6 @@ export function buildMutedRegex(serverSideFilters: mastodon.v2.Filter[]): RegExp
 export function extractMutedKeywords(serverSideFilters: mastodon.v2.Filter[]): string[] {
     let keywords = serverSideFilters.map(f => f.keywords.map(k => k.keyword)).flat().flat().flat();
     keywords = keywords.map(k => k.toLowerCase().replace(/^#/, ""));
-    logger.trace(`<mutedKeywords()> found ${keywords.length} keywords:`, keywords);
+    // logger.trace(`<mutedKeywords()> found ${keywords.length} keywords:`, keywords);
     return keywords;
 };
