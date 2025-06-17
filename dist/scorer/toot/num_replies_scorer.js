2f5e67bff1188dee9cfb5b92bdbb5a067928fc74
ashariyar <ashariyar@users.noreply.github.com>
2025-06-17 01:49:12 -0400

    jsdoc

--- dist/scorer/toot/num_replies_scorer.js
+++ dist/scorer/toot/num_replies_scorer.js
@@ -3,11 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/*
- * Score how many times the toot has been replied to by other users.
+/**
+ * @module scorers
  */
 const toot_scorer_1 = __importDefault(require("../toot_scorer"));
 const enums_1 = require("../../enums");
+/**
+ * Score how many times the toot has been replied to by other users.
+ */
 class NumRepliesScorer extends toot_scorer_1.default {
     description = "Favour toots with lots of replies";
     constructor() {
--- src/scorer/toot/num_replies_scorer.ts
+++ src/scorer/toot/num_replies_scorer.ts
@@ -1,11 +1,14 @@
-/*
- * Score how many times the toot has been replied to by other users.
+/**
+ * @module scorers
  */
 import TootScorer from '../toot_scorer';
 import Toot from '../../api/objects/toot';
 import { ScoreName } from '../../enums';
 
 
+/**
+ * Score how many times the toot has been replied to by other users.
+ */
 export default class NumRepliesScorer extends TootScorer {
     description = "Favour toots with lots of replies";
 
