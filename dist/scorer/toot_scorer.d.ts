cb2da907d9263140f2f513592d1daf234527b6c9
ashariyar <ashariyar@users.noreply.github.com>
2025-06-17 02:26:30 -0400

    jsdoc namespace

--- dist/scorer/toot_scorer.d.ts
+++ dist/scorer/toot_scorer.d.ts
@@ -1,14 +1,16 @@
-/**
- * @module toot_scorers
- */
 import Scorer from "./scorer";
 import { ScoreName } from '../enums';
 import { type StringNumberDict } from "../types";
+/**
+ * Namespace for Scorers that operate on a Toot independent of the rest of the feed.
+ * @namespace toot_scorers
+ */
 /**
  * Base class for a Scorer that can score a toot based solely on the properties of that
  * toot, optionally coupled with the fedialgo user's account data. Most importantly a
  * TootScorer does *not* require information about any other Toots in the feed (unlike a FeedScorer,
  * which requires knowledge of the entire timeline to render a score).
+ * @memberof toot_scorers
  */
 export default abstract class TootScorer extends Scorer {
     constructor(scoreName: ScoreName);
--- src/scorer/toot_scorer.ts
+++ src/scorer/toot_scorer.ts
@@ -1,18 +1,20 @@
-/**
- * @module toot_scorers
- */
 import Scorer from "./scorer";
 import { ageString } from "../helpers/time_helpers";
 import { isDebugMode } from "../helpers/environment_helpers";
 import { ScoreName } from '../enums';
 import { type StringNumberDict } from "../types";
 
+/**
+ * Namespace for Scorers that operate on a Toot independent of the rest of the feed.
+ * @namespace toot_scorers
+ */
 
 /**
  * Base class for a Scorer that can score a toot based solely on the properties of that
  * toot, optionally coupled with the fedialgo user's account data. Most importantly a
  * TootScorer does *not* require information about any other Toots in the feed (unlike a FeedScorer,
  * which requires knowledge of the entire timeline to render a score).
+ * @memberof toot_scorers
  */
 export default abstract class TootScorer extends Scorer {
     constructor(scoreName: ScoreName) {
