ac4c5c5819675eea84da564865ad3fccf5affdbb
ashariyar <ashariyar@users.noreply.github.com>
2025-06-17 23:10:19 -0400

    UnknownError type

--- dist/api/errors.d.ts
+++ dist/api/errors.d.ts
@@ -3,30 +3,32 @@
  * @module api_errors
  */
 import { Logger } from "../helpers/logger";
+type UnknownError = Error | unknown;
 /**
  * Returns true if the error is an access token revoked error.
- * @param {Error | unknown} e - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @returns {boolean} True if the error is an access token revoked error.
  */
-export declare function isAccessTokenRevokedError(e: Error | unknown): boolean;
+export declare function isAccessTokenRevokedError(error: UnknownError): boolean;
 /**
  * Returns true if the error is a rate limit error.
- * @param {Error | unknown} e - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @returns {boolean} True if the error is a rate limit error.
  */
-export declare function isRateLimitError(e: Error | unknown): boolean;
+export declare function isRateLimitError(error: UnknownError): boolean;
 /**
  * Throws if the error is an access token revoked error, otherwise logs and moves on.
  * @param {Logger} logger - Logger instance.
- * @param {unknown} error - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @param {string} msg - Message to log.
  * @throws {unknown} If the error is an access token revoked error.
  */
-export declare function throwIfAccessTokenRevoked(logger: Logger, error: unknown, msg: string): void;
+export declare function throwIfAccessTokenRevoked(logger: Logger, error: UnknownError, msg: string): void;
 /**
  * Throws a sanitized rate limit error if detected, otherwise logs and throws the original error.
- * @param {unknown} error - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @param {string} msg - Message to log.
  * @throws {string|unknown} Throws a user-friendly rate limit warning or the original error.
  */
-export declare function throwSanitizedRateLimitError(error: unknown, msg: string): void;
+export declare function throwSanitizedRateLimitError(error: UnknownError, msg: string): void;
+export {};
--- src/api/errors.ts
+++ src/api/errors.ts
@@ -6,47 +6,49 @@
 import { apiLogger, ACCESS_TOKEN_REVOKED_MSG, RATE_LIMIT_ERROR_MSG } from "./api";
 import { Logger } from "../helpers/logger";
 
+type UnknownError = Error | unknown;
+
 const RATE_LIMIT_USER_WARNING = "Your Mastodon server is complaining about too many requests coming too quickly. Wait a bit and try again later.";
 
 
 /**
  * Returns true if the error is an access token revoked error.
- * @param {Error | unknown} e - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @returns {boolean} True if the error is an access token revoked error.
  */
-export function isAccessTokenRevokedError(e: Error | unknown): boolean {
-    if (!(e instanceof Error)) {
-        apiLogger.warn(`error 'e' is not an instance of Error:`, e);
+export function isAccessTokenRevokedError(error: UnknownError): boolean {
+    if (!(error instanceof Error)) {
+        apiLogger.warn(`error 'e' is not an instance of Error:`, error);
         return false;
     }
 
-    return e.message.includes(ACCESS_TOKEN_REVOKED_MSG);
+    return error.message.includes(ACCESS_TOKEN_REVOKED_MSG);
 };
 
 
 /**
  * Returns true if the error is a rate limit error.
- * @param {Error | unknown} e - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @returns {boolean} True if the error is a rate limit error.
  */
-export function isRateLimitError(e: Error | unknown): boolean {
-    if (!(e instanceof Error)) {
-        apiLogger.warn(`error 'e' is not an instance of Error:`, e);
+export function isRateLimitError(error: UnknownError): boolean {
+    if (!(error instanceof Error)) {
+        apiLogger.warn(`error 'e' is not an instance of Error:`, error);
         return false;
     }
 
-    return e.message.includes(RATE_LIMIT_ERROR_MSG);
+    return error.message.includes(RATE_LIMIT_ERROR_MSG);
 };
 
 
 /**
  * Throws if the error is an access token revoked error, otherwise logs and moves on.
  * @param {Logger} logger - Logger instance.
- * @param {unknown} error - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @param {string} msg - Message to log.
  * @throws {unknown} If the error is an access token revoked error.
  */
-export function throwIfAccessTokenRevoked(logger: Logger, error: unknown, msg: string): void {
+export function throwIfAccessTokenRevoked(logger: Logger, error: UnknownError, msg: string): void {
     logger.error(`${msg}. Error:`, error);
     if (isAccessTokenRevokedError(error)) throw error;
 };
@@ -54,11 +56,11 @@ export function throwIfAccessTokenRevoked(logger: Logger, error: unknown, msg: s
 
 /**
  * Throws a sanitized rate limit error if detected, otherwise logs and throws the original error.
- * @param {unknown} error - The error to check.
+ * @param {UnknownError} error - The error to check.
  * @param {string} msg - Message to log.
  * @throws {string|unknown} Throws a user-friendly rate limit warning or the original error.
  */
-export function throwSanitizedRateLimitError(error: unknown, msg: string): void {
+export function throwSanitizedRateLimitError(error: UnknownError, msg: string): void {
     if (isRateLimitError(error)) {
         apiLogger.error(`Rate limit error:`, error);
         throw RATE_LIMIT_USER_WARNING;
