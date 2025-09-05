21d97891ae1f210076d48eea69f79d1c3928d7fe
ashariyar <ashariyar@users.noreply.github.com>
2025-09-05 03:30:27 -0400

    Failed attempt at getEnvVars()

--- src/helpers/environment_helpers.ts
+++ src/helpers/environment_helpers.ts
@@ -4,19 +4,9 @@
 
 import { FEDIALGO, bracketed } from "./string_helpers";
 
+const BRACKETED_FEDIALGO = bracketed(FEDIALGO);
+const ENV_VARS_TO_LOG = ['NODE_ENV', 'FEDIALGO_DEBUG', 'FEDIALGO_DEEP_DEBUG', 'QUICK_MODE', 'LOAD_TEST'];
 
-const bracketedFedialgo = bracketed(FEDIALGO);
-const logLineJoiner = '\n' + ' '.repeat(bracketedFedialgo.length + 1);
-
-const envVarsToLog = [
-    `${bracketedFedialgo} NODE_ENV="${process.env.NODE_ENV}"`,
-    `FEDIALGO_DEBUG="${process.env.FEDIALGO_DEBUG}"`,
-    `FEDIALGO_DEEP_DEBUG="${process.env.FEDIALGO_DEEP_DEBUG}"`,
-    `QUICK_MODE=${process.env.QUICK_MODE}`,
-    `LOAD_TEST=${process.env.LOAD_TEST}`
-];
-
-console.log(envVarsToLog.join(logLineJoiner));
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const isProduction = process.env.NODE_ENV === "production";
@@ -31,3 +21,35 @@ export const isDebugMode = isDeepDebug || process.env.FEDIALGO_DEBUG === "true";
 export const isLoadTest = process.env.LOAD_TEST === "true";
 // Set for a much shorter startup time, useful for development and testing
 export const isQuickMode = process.env.QUICK_MODE === "true";
+
+
+/**
+ * Read array of environment variables and return them as a dictionary.
+ * @param {string[]} varNames - Array of environment variable names to get
+ * @returns {Record<string, string | undefined>} Dictionary of env var names to their values (undefined if not set)
+ */
+export function getEnvVars(varNames: string[]): Record<string, string | undefined> {
+    return varNames.reduce((dict, v) => {
+        const envVar = process.env[v];
+        const fedialgo_debug = 'FEDIALGO_DEBUG';
+        console.log(`getEnvVars: checking for "${v}" in process.env (value = "${envVar}")
+            process.env.FEDIALGO_DEBUG=${process.env.FEDIALGO_DEBUG}
+            process.env['FEDIALGO_DEBUG']=${process.env['FEDIALGO_DEBUG']}
+            fedialgo_debug="${fedialgo_debug}"
+            process.env[fedialgo_debug]=${process.env[fedialgo_debug]}
+            dict is now:`, dict);
+        return {...dict, [v]: process.env[v]};
+     }, {});
+};
+
+
+const envVars = {
+
+};
+
+const envVarLogLines = Object.entries(envVars).map(([k, v]) => `${k}="${v}"`);
+console.log(BRACKETED_FEDIALGO + ' ' + envVarLogLines.join('\n' + ' '.repeat(BRACKETED_FEDIALGO.length + 1)));
+console.log("process.env.NODE_ENV =", process.env.NODE_ENV);
+console.log("process.env['NODE_ENV'] =", process.env["NODE_ENV"]);
+console.log(`envVars: `, envVars);
+console.log(`process.env: `, process.env);
