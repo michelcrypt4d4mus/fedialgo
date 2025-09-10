ddc8271211e27cf04da2557dc52b5373f11ae6e0
ashariyar <ashariyar@users.noreply.github.com>
2025-09-09 20:51:50 -0400

    JSDocs

--- dist/scorer/scorer_cache.d.ts
+++ dist/scorer/scorer_cache.d.ts
@@ -1,11 +1,21 @@
 import type FeedScorer from "./feed_scorer";
 import type Scorer from "./scorer";
 import type TootScorer from "./toot_scorer";
+/**
+ * Class that exists to avoid circular dependencies so Scorer can access the weights in
+ * {@linkcode TheAlgorithm} instance.
+ */
 export default class ScorerCache {
     static feedScorers: FeedScorer[];
     static tootScorers: TootScorer[];
     static weightedScorers: Scorer[];
     static addScorers(tootScorers: TootScorer[], feedScorers: FeedScorer[]): void;
+    /**
+     * Prepare the {@linkcode Scorer}s for scoring. If {@linkcode force} is true force recompute of
+     * all {@linkcode Scorer}'s {@linkcode scoringData} dictionaries.
+     * @param {boolean} [force] - Whether to force recompute of {@linkcode Scorer.scoringData}.
+     */
     static prepareScorers(force?: boolean): Promise<void>;
+    /** Reset all the {@linkcode Scorer}'s internal state. */
     static resetScorers(): void;
 }
