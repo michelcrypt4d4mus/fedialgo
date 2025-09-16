56491e2ee851837035fbf06c31bee4bd5d82d24a
ashariyar <ashariyar@users.noreply.github.com>
2025-09-15 21:50:33 -0400

    MastoApi docstrings

--- dist/api/api.d.ts
+++ dist/api/api.d.ts
@@ -191,7 +191,7 @@ export default class MastoApi {
      */
     getStatusesForTag(tagName: string, logger: Logger, numToots: number): Promise<TootLike[]>;
     /**
-     * Retrieves background data about the user for scoring, etc. Caches as an instance variable.
+     * Retrieves background data about the user for scoring, etc. Cached as an instance variable.
      * @param {boolean} [force] - If true, forces a refresh from the API.
      * @returns {Promise<UserData>} The UserData object.
      */
@@ -214,8 +214,8 @@ export default class MastoApi {
      */
     instanceInfo(): Promise<mastodon.v2.Instance>;
     /**
-     * Return true if the user's home server is a GoToSocial server. Has side effect of setting
-     * the isHomeserverGoToSocial property on this MastoApi object.
+     * Return {@linkcode true} if the user's home server is a GoToSocial server. Has side effect of setting
+     * {@linkcode this.isHomeserverGoToSocial} property on this {@linkcode MastoApi} instance.
      * @returns {Promise<boolean>}
      */
     isGoToSocialUser(): Promise<boolean>;
@@ -226,10 +226,9 @@ export default class MastoApi {
     lockAllMutexes(): Promise<ConcurrencyLockRelease[]>;
     /**
      * Resolves a foreign server toot URI to one on the user's local server using the v2 search API.
-     * transforms URLs like this: https://fosstodon.org/@kate/114360290341300577
-     *                   to this: https://universeodon.com/@kate@fosstodon.org/114360290578867339
      * @param {Toot} toot - The toot to resolve.
      * @returns {Promise<Toot>} The resolved toot.
+     * @example "https://fosstodon.org/@kate/114360290341300577" => "https://universeodon.com/@kate@fosstodon.org/114360290578867339"
      */
     resolveToot(toot: Toot): Promise<Toot>;
     /**
@@ -241,7 +240,7 @@ export default class MastoApi {
      */
     searchForToots(searchStr: string, logger: Logger, maxRecords?: number): Promise<mastodon.v1.Status[]>;
     /**
-     * Records an API error by logging it and adding it to the apiErrors array.
+     * Records an API error by logging it and adding it to the {@linkcode this.apiErrors} array.
      * @param {string} msg - Message to log
      * @param {Error} [causedByError] - The underlying Error object that caused this error, if any.
      * @param {Logger} [logger] - Logger instance to use (defaults to this.logger)
@@ -258,12 +257,12 @@ export default class MastoApi {
     setSemaphoreConcurrency(concurrency: number): void;
     /**
      * Returns the URL for an account on the Feialgo user's home server.
-     * @param {Hashtag | string} tag - The tag or tag object.
+     * @param {Account} account - The tag or tag object.
      * @returns {string} The tag URL.
      */
     accountUrl(account: Account): string;
     /**
-     * Returns true if the URL is a local URL on the Feialgo user's home server.
+     * Returns {@linkcode true} if the URL is a local URL on the Feialgo user's home server.
      * @param {string} url - URL to check
      * @returns {boolean}
      */
